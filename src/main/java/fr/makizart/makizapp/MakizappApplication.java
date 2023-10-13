80607e37565eea4cb4a029ec3484697c9be6b46e
happy44300 <23211462+happy44300@users.noreply.github.com>
2023-10-13 18:14:12 +0200

    Initialize project

---
+++ src/main/java/fr/makizart/makizapp/MakizappApplication.java
@@ -0,0 +1,13 @@
+package fr.makizart.makizapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakizappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MakizappApplication.class, args);
+	}
+
+}
---
+++ src/test/java/fr/makizart/makizapp/MakizappApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.makizart.makizapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakizappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
