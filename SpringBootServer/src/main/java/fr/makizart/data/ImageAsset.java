6d2311f4d5962138b73ab1fa4a2908243d027524
happy44300 <116593274+motivational-dragon@users.noreply.github.com>
2023-10-29 18:22:44 +0100

    Add database in docker-compose.yml

---
+++ SpringBootServer/src/main/java/fr/makizart/data/ImageAsset.java
@@ -0,0 +1,4 @@
+package fr.makizart.data;
+
+public class ImageAsset {
+}
---
+++ SpringBootServer/src/main/java/fr/makizart/data/Project.java
@@ -0,0 +1,9 @@
+package fr.makizart.data;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "Project")
+public class Project {
+}
