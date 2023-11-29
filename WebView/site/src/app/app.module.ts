29a944e041c6764379dd94c013d195eeb81eeba7
Rod4401 <m.meunier.rodrigue@gmail.com>
2023-11-29 10:55:11 +0100

    update webview --> indent and remove budgets

--- WebView/site/src/app/app.module.ts
+++ WebView/site/src/app/app.module.ts
@@ -9,34 +9,35 @@ import {TopBarComponent} from './admin/top-bar/top-bar.component';
 import {SafePipe} from "./admin/commons/safe.pipe";
 import {FormsModule} from "@angular/forms";
 import {HttpClientModule} from "@angular/common/http";
-import { AppRoutingModule } from './app-routing.module';
-import { AdminComponent } from './admin/admin.component';
-import { UserComponent } from './user/user.component';
-import {RouterOutlet} from "@angular/router";
-import { LoginComponent } from './login/login.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AdminComponent} from './admin/admin.component';
+import {UserComponent} from './user/user.component';
+import {RouterModule, RouterOutlet} from "@angular/router";
+import {LoginComponent} from './login/login.component';
 
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        ProjectManagerComponent,
-        ProjectEditorComponent,
-        TopBarComponent,
-        SafePipe,
-        AdminComponent,
-        UserComponent,
-        LoginComponent,
-    ],
+  declarations: [
+    AppComponent,
+    ProjectManagerComponent,
+    ProjectEditorComponent,
+    TopBarComponent,
+    SafePipe,
+    AdminComponent,
+    UserComponent,
+    LoginComponent,
+  ],
   imports: [
     BrowserModule,
     NgbModule,
     FormsModule,
     HttpClientModule,
     AppRoutingModule,
+    RouterModule,
     RouterOutlet,
   ],
-    providers: [],
-    bootstrap: [AppComponent]
+  providers: [],
+  bootstrap: [AppComponent]
 })
 export class AppModule {
 }
